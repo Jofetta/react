bec7cd7f593fbe52f870cc5095a068e70a723873
jofetta <jafarovatatiana@gmail.com>
2024-07-14 00:41:38 +0300

    feat: add pagination

--- class-components/src/components/Page.tsx
+++ class-components/src/components/Page.tsx
@@ -6,16 +6,20 @@ import CardsContainer from './CardsContainer';
 import ErrorBoundary from './ErrorBoundary';
 import ErrorButton from './ErrorButton';
 import useLocalStorage from '../utils/localStorage';
+import PaginationButton from './PaginationButton';
 
 export type ApiData = {
   query?: string;
   results?: [];
+  next?: string;
+  previous?: string;
 };
 export default function Page() {
   const { query, setQuery } = useLocalStorage('pokemonQuery', '');
   const [tempQuery, setTempQuery] = useState('');
-  const [apiData, setApiData] = useState({});
+  const [apiData, setApiData] = useState<ApiData>({});
   const [isLoading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
 
   function updateState(searchString: string) {
     setTempQuery(searchString);
@@ -54,6 +58,19 @@ export default function Page() {
     setLoading(false);
   }
 
+  async function changePage(forward: boolean) {
+    console.log(apiData);
+    if (forward) {
+      setCurrentPage(currentPage + 1);
+      const data = await fetchData(apiData.next);
+      setApiData(data);
+    } else {
+      setCurrentPage(currentPage - 1);
+      const data = await fetchData(apiData.previous);
+      setApiData(data);
+    }
+  }
+
   const props: ButtonProps = {
     callback: () => handleClick(),
   };
@@ -74,12 +91,27 @@ export default function Page() {
       <ErrorBoundary fallback={<div>Something went wrong</div>}>
         <section>
           {apiData && (
-            <CardsContainer
-              key={`key_${isLoading}_${query}`}
-              isLoading={isLoading}
-              query={query ? query : ''}
-              apiData={apiData}
-            />
+            <div>
+              <CardsContainer
+                key={`key_${isLoading}_${query}`}
+                isLoading={isLoading}
+                query={query ? query : ''}
+                apiData={apiData}
+              />
+              <div className="pagination-container">
+                <PaginationButton
+                  classList="pagination-back"
+                  callback={() => changePage(false)}
+                  currentPage={currentPage}
+                />
+                <div>{currentPage}</div>
+                <PaginationButton
+                  classList="pagination-forward"
+                  callback={() => changePage(true)}
+                  currentPage={currentPage}
+                />
+              </div>
+            </div>
           )}
           {apiData === undefined && <div>No results</div>}
         </section>
