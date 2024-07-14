d24863038600455ce7849abb20119592bcb27325
jofetta <jafarovatatiana@gmail.com>
2024-07-15 00:20:34 +0300

    feat: add vitest and some tests

--- class-components/src/pages/NotFoundPage.tsx
+++ class-components/src/pages/NotFoundPage.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFoundPage() {
   return (
     <>
       <h1>404</h1>
       <p>The page is not found</p>
+      <Link to="/">Go to home page</Link>
     </>
   );
 }
